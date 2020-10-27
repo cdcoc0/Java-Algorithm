3a9031781dec5356744cfe50e38ecfd3f32c4e29
velykitty <jiyunee13@gmail.com>
2020-10-27 11:54:21 +0900

    IntQueue

--- src/com/kiri/chop/chapter04/queue/IntQueue.java
+++ src/com/kiri/chop/chapter04/queue/IntQueue.java
@@ -28,5 +28,75 @@ public class IntQueue {
 		}
 	}
 	
+	public int enque(int x) throws OverflowIntQueueException {
+		if(num >= max)
+			throw new OverflowIntQueueException();
+		
+		que[rear++] = x;
+		num++;
+		
+		if(rear == max)
+			rear = 0;
+		
+		return x;
+	}
 	
+	public int deque() throws EmptyIntQueueException {
+		if(num <= 0)
+			throw new EmptyIntQueueException();
+		
+		int x = que[front++];
+		num--;
+		
+		if(front == max)
+			front = 0;
+		
+		return x;
+	}
+	
+	public int peek() throws EmptyIntQueueException {
+		if(num <= 0)
+			throw new EmptyIntQueueException();
+		
+		return que[front];
+	}
+	
+	public int indexOf(int x) {
+		for(int i = 0; i < num; i++) {
+			int idx = (i + front) % max;
+			if(que[idx] == x)
+				return idx;
+		}
+		return -1;
+	}
+	
+	public void clear() {
+		num = front = rear = 0;
+	}
+	
+	public int capacity() {
+		return max;
+	}
+	
+	public int size() {
+		return num;
+	}
+	
+	public boolean IsEmpty() {
+		return num <= 0;
+	}
+	
+	public boolean IsFull() {
+		return num >= max;
+	}
+	
+	public void dump() {
+		if(num <= 0)
+			System.out.println("the queue is empty");
+		else {
+			for(int i = 0; i < num; i++) {
+				System.out.println(que[(i + front) % max]);
+			}
+		}
+	}
 }
