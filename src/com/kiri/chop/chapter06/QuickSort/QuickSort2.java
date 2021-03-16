945923bad0bd3f2a14e67d1ef25308140cadd479
velykitty <jiyunee13@gmail.com>
2021-03-16 22:42:16 +0900

    6-6. quickSort

---
+++ src/com/kiri/chop/chapter06/QuickSort/QuickSort2.java
@@ -0,0 +1,25 @@
+package com.kiri.chop.chapter06.QuickSort;
+
+public class QuickSort2 {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	static void quickSort2(int[] a, int n) {
+		int pl = 0;
+		int pr = n - 1;
+		int x = a[(pl + pr) / 2];
+		
+		do {
+			while(a[pl] < x) pl++;
+			while(a[pr] > x) pr--;
+			if(pl <= pr)
+				swap(a, pl++, pr--);
+		} while(pl <= pr);
+		
+		if(0 < pr) quickSort2(a, pr);
+		if(pl < n - 1) quickSort2(a, n);
+	}
+}
